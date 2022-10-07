ef0ab26519417787638e437d90d46072bb6b94b3
yourusername <nick_gayem@yahoo.com>
2022-10-06 23:12:04 -0700

    Adding functionality for BMI Calculator

--- BMI Calculator Mobile/View/ResultsViewController.swift
+++ BMI Calculator Mobile/View/ResultsViewController.swift
@@ -30,6 +30,8 @@ class ResultsViewController: UIViewController {
     
     /*
     // MARK: - Navigation
+     
+     
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
